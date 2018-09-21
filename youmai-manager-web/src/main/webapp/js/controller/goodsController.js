8f8f7b7549472a9f25bbf4137403b4c71dfc9e69
Giraffehjl <30527644+Giraffehjl@users.noreply.github.com>
2018-09-21 15:40:03 +0800

    商家商品列表页面多个删除

--- youmai-manager-web/src/main/webapp/js/controller/goodsController.js
+++ youmai-manager-web/src/main/webapp/js/controller/goodsController.js
@@ -1,80 +1,96 @@
- //控制层 
-app.controller('goodsController' ,function($scope,$controller   ,goodsService){	
-	
+ //控制层
+app.controller('goodsController' ,function($scope,$controller   ,itemService,goodsService){
+
 	$controller('baseController',{$scope:$scope});//继承
-	
+
     //读取列表数据绑定到表单中  
 	$scope.findAll=function(){
 		goodsService.findAll().success(
 			function(response){
 				$scope.list=response;
-			}			
+			}
 		);
-	}    
-	
+	}
+
 	//分页
-	$scope.findPage=function(page,rows){			
+	$scope.findPage=function(page,rows){
 		goodsService.findPage(page,rows).success(
 			function(response){
-				$scope.list=response.rows;	
+				$scope.list=response.rows;
 				$scope.paginationConf.totalItems=response.total;//更新总记录数
-			}			
+			}
 		);
 	}
-	
-	//查询实体 
-	$scope.findOne=function(id){				
+
+	//查询实体
+	$scope.findOne=function(id){
 		goodsService.findOne(id).success(
 			function(response){
-				$scope.entity= response;					
+				$scope.entity= response;
 			}
-		);				
+		);
 	}
-	
-	//保存 
-	$scope.save=function(){				
-		var serviceObject;//服务层对象  				
+
+	//保存
+	$scope.save=function(){
+		var serviceObject;//服务层对象
 		if($scope.entity.id!=null){//如果有ID
-			serviceObject=goodsService.update( $scope.entity ); //修改  
+			serviceObject=goodsService.update( $scope.entity ); //修改
 		}else{
-			serviceObject=goodsService.add( $scope.entity  );//增加 
-		}				
+			serviceObject=goodsService.add( $scope.entity  );//增加
+		}
 		serviceObject.success(
 			function(response){
 				if(response.success){
-					//重新查询 
+					//重新查询
 		        	$scope.reloadList();//重新加载
 				}else{
 					alert(response.message);
 				}
-			}		
-		);				
+			}
+		);
 	}
-	
-	 
-	//批量删除 
-	$scope.dele=function(){			
-		//获取选中的复选框			
+
+
+	//批量删除
+	$scope.dele=function(){
+		//获取选中的复选框
 		goodsService.dele( $scope.selectIds ).success(
 			function(response){
 				if(response.success){
 					$scope.reloadList();//刷新列表
 					$scope.selectIds=[];
-				}						
-			}		
-		);				
-	}
-	
-	$scope.searchEntity={};//定义搜索对象 
-	
-	//搜索
-	$scope.search=function(page,rows){			
-		goodsService.search(page,rows,$scope.searchEntity).success(
-			function(response){
-				$scope.list=response.rows;	
-				$scope.paginationConf.totalItems=response.total;//更新总记录数
-			}			
+				}
+			}
 		);
 	}
-    
+
+	$scope.searchEntity={};//定义搜索对象
+
+	//搜索
+	$scope.search=function(page,rows){
+		goodsService.search(page,rows,$scope.searchEntity).success(
+			function(response){
+				$scope.list=response.rows;
+				$scope.paginationConf.totalItems=response.total;//更新总记录数
+			}
+		);
+	}
+
+	//商品状态审核
+    $scope.status=['未审核', '已审核', '审核未通过', '关闭'];
+	//商品列表
+	$scope.itemList=[];
+	//查询商品分类
+    $scope.findItemList=function () {
+        itemService.findAll().success(
+            function (response) {
+                for (var i = 0; i <response.length ; i++) {
+                    $scope.itemList[response[i].id]=response[i].name;
+                }
+
+            }
+        )
+    }
+
 });	
